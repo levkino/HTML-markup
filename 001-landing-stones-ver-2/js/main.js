ef5f3a4b6d38b884bfcab454849adc121d4cf4fb
Лев Ованоглян <levkino@yandex.ru>
2020-05-29 11:09:05 +0300

    Добавил сайт ПетроМрамора.

---
+++ 001-landing-stones-ver-2/js/main.js
@@ -0,0 +1,137 @@
+// const menu = document.querySelector('.menu');
+// const burger = document.querySelector('.burger');
+// burger.addEventListener('click', function() {
+//   this.classList.toggle('active');
+//   menu.classList.toggle('active');
+//   document.body.classList.toggle('lock');  
+// });
+
+// $('.box').click(function(){
+//     $(this).addClass('fileFocus');
+// });
+
+$(document).ready(function() {
+  // Инициализация wow-появлений
+  new WOW().init();
+
+  // Инициализация масок ввода
+  $('.phone-input').mask("+7 (000) 000-00-00", {placeholder: "+7 (___) ___-__-__"});
+
+  // Инициализация табов
+  $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+  });
+
+  // Инициализация сертификатов
+  $(".fancybox-button").fancybox();
+
+  // Инициализация галерей
+  $(".stoneBlocks").slick({
+    dots: false,
+    rows: 2,
+    infinite: true,
+    centerMode: true,
+    centerPadding: '1px',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [{ breakpoint: 651, settings: { slidesToShow: 3 }}]
+  });
+  $(".production__gallery").slick({ dots: true });
+});
+
+function scrollTo(selector) {
+  const top = $(selector).offset().top;
+  $('html, body').animate({ scrollTop: top }, 500);
+}
+
+$('a[href*="#"]').on('click', function(e) {
+  if (this.hash.length > 3) {
+    if (this.className.includes('menu')) {
+      const activeClass = 'header__menu-item-active';
+      $('.' + activeClass).toggleClass(activeClass);
+      this.classList.toggle(activeClass);
+    }
+    scrollTo(this.hash);
+  }
+})
+
+
+function callRequest() {
+  const name = document.querySelector('#call-name').value;
+  if (!name) {
+    // alert('Укажите имя');
+    // return;
+  }
+  const phone = document.querySelector('#call-phone').value; 
+  if (!phone) {
+    alert('Укажите телефон');
+    return;
+  }
+  const data = {
+    // recipient: 'tatyana-arh1@bk.ru',
+    recipient: 'levkino@yandex.ru',
+    // recipient: 'stones@petromramor.ru',
+    title: 'Заказ звонка',
+    fields: {
+      Имя: name,
+      Телефон: phone
+    }
+  };
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'php/mail-call.php');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onreadystatechange = () => {
+    if (xhr.status === 200) {
+      $('#modal-call').modal('hide');
+      $('#modal-thank-you').modal('show');
+    } else {
+      alert(`Ошибка отправки${xhr.response}`);
+    }
+  };
+  xhr.send(JSON.stringify(data));  
+  return false;
+}
+
+function calcRequest(formNumber) {
+  const question = document.querySelector(`#calc-question${formNumber}`).value;
+  if (!question) {
+    alert('Укажите вопрос');
+    return;
+  }
+
+  const email = document.querySelector(`#calc-phone${formNumber}`).value;
+  if (!email) {
+    alert('Укажите электронную почту');
+    return;
+  }
+  
+  const data = {
+    // recipient: 'tatyana-arh1@bk.ru',
+    recipient: 'levkino@yandex.ru',
+    // recipient: 'stones@petromramor.ru',
+    title: 'Заказ звонка',
+    fields: {
+      Вопрос: question,
+      'E-mail': emails
+    }
+  };
+  const fileInput = document.querySelector(`#calc-file${formNumber}`);
+  const formData = new FormData();
+  formData.append("data", JSON.stringify(data));
+  formData.append("file", fileInput.files[0]);
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'php/mail-calc.php');
+  xhr.onreadystatechange = () => {
+    if (xhr.status === 200) {
+      $('#modal-calc').modal('hide');
+      $('#modal-thank-you').modal('show');
+    } else {
+      alert(`Ошибка отправки${xhr.response}`);
+    }
+  };
+  xhr.send(formData);
+  return false;
+}
---
+++ 001-landing-stones-ver-2/php/mail-calc.php
@@ -0,0 +1,59 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type');
+
+$data = json_decode($_POST['data'], true);
+if (empty($data['message']))
+  $data['message'] = createMessage($data['fields']);
+sendMail($data);
+
+function createMessage($fields) {
+  $c = true;
+  $message = "";
+  foreach ($fields as $key => $value) {
+    $message .= "
+    " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+      <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+      <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+      </tr>";
+    }
+  return "<table style='width: 100%;'>$message</table>";
+}
+
+function sendMail($data) {
+  if (empty($data) || empty($data['recipient']) || empty($data['title']) || empty($data['message'])) {
+    echo 'Проверьте правильность переданных параметров.';
+    return;
+  }
+
+  $from = 'mail@onpix.ru';
+  $to = $data['recipient'];
+  $title = $data['title'];
+  $separator = md5(time());
+  $headers = "MIME-Version: 1.0" . PHP_EOL .
+             "Content-Type: multipart/mixed; charset=utf-8; boundary=\"" . $separator . "\"" . PHP_EOL .
+             "Content-Transfer-Encoding: 7bit" . PHP_EOL .
+             "FROM: Onpix <$from>";
+
+            // message
+  $body = "--" . $separator . PHP_EOL;
+  $body .= "Content-Type: text/html; charset=utf-8" . PHP_EOL;
+  $body .= "Content-Transfer-Encoding: 8bit" . PHP_EOL;
+  $body .= $data['message'] . PHP_EOL;
+
+  // attachment
+  $file = $_FILES['file'];
+  $content = file_get_contents($file['tmp_name']);
+  $content = chunk_split(base64_encode($content));
+  $body .= "--" . $separator . PHP_EOL;
+  $body .= "Content-Type: application/octet-stream; name=\"" . $file['name'] . "\"" . PHP_EOL;
+  $body .= "Content-Transfer-Encoding: base64" . PHP_EOL;
+  $body .= "Content-Disposition: attachment; filename=\"" . $file['name'] . "\"" . PHP_EOL;
+  $body .= $content . PHP_EOL;
+  $body .= "--" . $separator . "--";
+
+  var_dump(mail($to, $title, $body, $headers));
+}
+?>
