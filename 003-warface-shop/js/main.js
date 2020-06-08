2a847d5fa6577c1bf8a55e3a53cb914066bebdae
Лев Ованоглян <levkino@yandex.ru>
2020-06-08 13:52:56 +0300

    Доделал главную страницу.

--- 003-warface-shop/js/main.js
+++ 003-warface-shop/js/main.js
@@ -6,32 +6,6 @@ burger.addEventListener('click', function() {
   document.body.classList.toggle('lock');  
 });
 
-$('.telephoneInput').mask("+7 (000) 000-00-00", {placeholder: "+7 (___) ___-__-__"});
-
-$('.box').click(function(){
-    $(this).addClass('fileFocus');
-});
-
-$(document).on('ready', function() {
-  $(".stoneBlocks").slick({
-    dots: false,
-    rows: 2,
-    infinite: true,
-    centerMode: true,
-    centerPadding: '1px',
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 651,
-        settings: {
-          slidesToShow: 3
-        }
-      }
-    ]
-  });
-  $(".productionGallery").slick({
-    dots: true
-  });
-  $(".objectsGallery").slick();
+document.addEventListener("DOMContentLoaded", function() {
+  
 });
